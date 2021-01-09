57f7538ad95a240bc2084d766b7da193c77feab7
Gianluca Ruocco <g.ruocco@controlexpert.com>
2021-01-09 16:51:47 +0100

    fix tests

--- apps/api/src/app/app.service.spec.ts
+++ apps/api/src/app/app.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test } from '@nestjs/testing';
-
+import { version } from '../../../../package.json';
 import { AppService } from './app.service';
 
 describe('AppService', () => {
@@ -7,7 +7,7 @@ describe('AppService', () => {
 
   beforeAll(async () => {
     const app = await Test.createTestingModule({
-      providers: [AppService],
+      providers: [AppService]
     }).compile();
 
     service = app.get<AppService>(AppService);
@@ -15,7 +15,7 @@ describe('AppService', () => {
 
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
-      expect(service.getData()).toEqual({ message: 'Welcome to api!' });
+      expect(service.getVersion()).toEqual({ version });
     });
   });
 });
