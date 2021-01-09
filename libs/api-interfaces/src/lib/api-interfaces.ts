c87e5e500ffe85bd6ff902ccf34799be236824de
Gianluca Ruocco <g.ruocco@controlexpert.com>
2021-01-09 16:48:59 +0100

    fix imports

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,5 +1,7 @@
+import { Version as VersionDto } from 'apps/api/src/app/dto/version';
+import { UpsertTodo as UpsertTodoDto } from 'apps/api/src/app/todos/dto/upsert-todo';
 import { TodoEntity } from 'apps/api/src/app/todos/todo.entity';
 
 export type Todo = TodoEntity;
-export type UpsertTodo = UpsertTodoDto;
+export type UpsertTodos = UpsertTodoDto;
 export type Version = VersionDto;
